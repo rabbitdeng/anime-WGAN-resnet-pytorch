822346b2f9a7f9a75d3e7f5cc914192c4b09c77e
rabbitdeng <88369122+rabbitdeng@users.noreply.github.com>
2021-09-12 16:48:36 +0800

    Add files via upload
    fix, optimize model structures. add more training tricks.

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ nz = 100
 nc = 3
 M = 32
 
-batch_size = 16  #设置一个batch的大小，注意与train.py中相同
+batch_size = 8  #设置一个batch的大小，注意与train.py中相同
 
 class BasicBlock(nn.Module):
     def __init__(self, in1):  #in1为输入的channel大小，BasicBlock输出等于输入channel大小
@@ -34,12 +34,12 @@ class BasicBlock(nn.Module):
       #  out = self.bn2(out)
         out = self.relu2(out)
 
-        out += residual
+        out = out + residual
         return out
 
-class RestNet18(nn.Module):
+class netD(nn.Module):
     def __init__(self):
-        super(RestNet18, self).__init__()
+        super(netD, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3 ,stride=1, padding=1)
 
 
@@ -47,41 +47,29 @@ class RestNet18(nn.Module):
             BasicBlock(64),
             nn.AvgPool2d(3, 2),
             BasicBlock(64),
-
-            BasicBlock(64),
-
         )
 
         self.layer2 = nn.Sequential(
             nn.AvgPool2d(3,2),
-            BasicBlock(64),
-
-            BasicBlock(64),
-                                   )
+            BasicBlock(64)
+        )
 
         self.layer3 = nn.Sequential(
             nn.AvgPool2d(3, 2),
             BasicBlock(64),
-
-            BasicBlock(64),
-                                    )
+        )
 
         self.layer4 = nn.Sequential(
             nn.AvgPool2d(3, 2),
             BasicBlock(64),
-            BasicBlock(64)
-          #  nn.LayerNorm([64,5,5]),
-          )
+        )
 
         self.layer5 = nn.Sequential(
             nn.BatchNorm2d(64),
-         #   nn.LayerNorm([64,5,5]),
             nn.ReLU(True)
         )
         self.fc = nn.Sequential(
-
-          nn.Linear(1600, 1),
-
+          nn.Linear(576, 1),
         )
     def forward(self, x):
         out = self.conv1(x)
@@ -90,16 +78,16 @@ class RestNet18(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = self.layer5(out)
-        out = torch.flatten(out,start_dim=1)
+        out = torch.flatten(out, start_dim=1)
         out = self.fc(out)
-        out = F.sigmoid(out)
+        out = torch.sigmoid(out)
         return out
 
 
-class Generator(nn.Module):
+class netG(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear = nn.Linear(nz, 64*3*3)
+        self.linear = nn.Linear(nz, 64*2*2)
         self.layer1 = nn.Sequential(
             BasicBlock(64),
             nn.UpsamplingNearest2d(scale_factor=2),
@@ -110,19 +98,14 @@ class Generator(nn.Module):
         self.layer2 = nn.Sequential(
             BasicBlock(64),
             nn.UpsamplingNearest2d(scale_factor=2),
-            BasicBlock(64),
-            nn.UpsamplingNearest2d(scale_factor=2)
-
         )
         self.layer3 = nn.Sequential(
             BasicBlock(64),
-            BasicBlock(64),
             nn.UpsamplingNearest2d(scale_factor=2)
-
         )
         self.layer4 = nn.Sequential(
             BasicBlock(64),
-
+            nn.UpsamplingNearest2d(scale_factor=2)
         )
         self.Conv = nn.Sequential(
             BasicBlock(64),
@@ -136,7 +119,7 @@ class Generator(nn.Module):
 
     def forward(self, z):
         x = self.linear(z)
-        x = x.view(batch_size,64,3,3)
+        x = x.view(batch_size,64,2,2)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
--- train.py
+++ train.py
@@ -3,10 +3,10 @@ import torch
 import torchvision
 import torchvision.utils as vutils
 import torch.nn as nn
-
-from model import RestNet18, Generator
-from losses import Wasserstein
-
+from tqdm import tqdm
+from model import netD,netG
+from losses import Hinge
+import os
 #已将数据集上传至百度网盘，连接如下：
 #链接：https://pan.baidu.com/s/1FWSmO5ZClyDy7YIlFwY7pw
 #提取码：wwdy
@@ -23,14 +23,17 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batchSize', type=int, default=16)
-parser.add_argument('--imageSize', type=int, default=96)
+parser.add_argument('--batchSize', type=int, default=8)
+parser.add_argument('--imageSize', type=int, default=64)
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--epoch', type=int, default=2000, help='number of epochs to train for')
+parser.add_argument('--epoch', type=int, default=300, help='number of epochs to train for')
 parser.add_argument('--lrd', type=float, default=5e-5, help="Discriminator's learning rate, default=0.00005")  #Discriminator's learning rate
 parser.add_argument('--lrg', type=float, default=5e-5, help="Generator's learning rate, default=0.00005")  #Generator's learning rate
 parser.add_argument('--data_path', default='data/', help='folder to train data')#将数据集放在此处
@@ -54,67 +57,92 @@ dataloader = torch.utils.data.DataLoader(
     shuffle=True,
     drop_last=True,
 )
-netG = Generator().to(device)
+netG = netG().to(device)
 netG.apply(weights_init)
 print(netG)
 
-netD = RestNet18().to(device)
+netD = netD().to(device)
 netD.apply(weights_init)
 print(netD)
 
 print(dataset)
 
-#netG.load_state_dict(torch.load('resnetimg/netG_0025.pth', map_location=device))#这两句用来读取预训练模型
-#netD.load_state_dict(torch.load('resnetimg/netD_0025.pth', map_location=device))#这两句用来读取预训练模型
-criterionG = Hinge()#use Hinge to avoid gredient icrease drasticly
+criterion = Hinge()#use Hinge to avoid gredient icrease drasticly
 optimizerG = torch.optim.RMSprop(netG.parameters(), lr=opt.lrg)
 optimizerD = torch.optim.RMSprop(netD.parameters(), lr=opt.lrd)
-criterionD = Hinge()
-label = torch.FloatTensor(opt.batchSize)
-real_label = 1
-fake_label = 0
 
-label = label.unsqueeze(1)
+lrd_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizerD, T_max=5, eta_min=5E-6)
+lrg_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizerG, T_max=5, eta_min=5E-6)
 
-start_epoch = 20  #设置初始epoch大小
+RESUME = False
+if RESUME:
+    path_checkpointD = "./checkpoint/ckpt_latestD_1.pth"  # 断点路径
+    path_checkpointG = "./checkpoint/ckpt_latestG_1.pth"  # 断点路径
+    checkpointD = torch.load(path_checkpointD)  # 加载断点
+    checkpointG = torch.load(path_checkpointG)  # 加载断点
+    netD.load_state_dict(checkpointD['net'])  # 加载模型可学习参数
+
+    optimizerD.load_state_dict(checkpointD['optimizer'])  # 加载优化器参数
+    start_epoch = checkpointD['epoch']  # 设置开始的epoch
+
+
+
+start_epoch = 0 #设置初始epoch大小
 
 for epoch in range(start_epoch + 1, opt.epoch + 1):
-    for i, (imgs, _) in enumerate(dataloader):
-        # 固定生成器G，训练鉴别器D
-        # 让D尽可能的把真图片判别为1
-        imgs = imgs.to(device)
-        outputreal = netD(imgs)
-        # label.data.fill_(real_label)
-        # label = label.to(device)
-        # label = label
-        optimizerD.zero_grad()
-        ## 让D尽可能把假图片判别为0
-        # label.data.fill_(fake_label)
-        noise = torch.randn(opt.batchSize, opt.nz)
-        # noise = torch.randn(opt.batchSize, opt.nz)
-        noise = noise.to(device)
-        # noise = noise
-        fake = netG(noise)  # 生成假图
-        outputfake = netD(fake.detach())  # 避免梯度传到G，因为G不用更新
-        lossD = criterionD(outputreal, outputfake)
-        lossD.backward()
-        optimizerD.step()
-        # 固定鉴别器D，训练生成器G
-        optimizerG.zero_grad()
-        # 让D尽可能把G生成的假图判别为1
-        # label.data.fill_(real_label)
-        # label = label.to(device)
-        # label = label
-        output = netD(fake)
-        lossG = criterionG(output)
-        lossG.backward()
-        optimizerG.step()
-        print('[%d/%d][%d/%d] Loss_D: %.3f Loss_G %.3f'
-              % (epoch, opt.epoch, i, len(dataloader), lossD.item(), lossG.item()))
-    vutils.save_image(fake.data,
-                      '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
-                      normalize=True)
+    with tqdm(total=len(dataloader), desc=f'Epoch {epoch}/{opt.epoch}', postfix=dict, mininterval=0.3) as pbar:
+        for i, (imgs, _) in enumerate(dataloader):
+            # 固定生成器G，训练鉴别器D
+            imgs = imgs.to(device)
+            outputreal = netD(imgs)
 
-    if epoch % 5 == 0:#每5个epoch，保存一次模型参数.
-        torch.save(netG.state_dict(), '%s/netG_%04d.pth' % (opt.outf, epoch))
-        torch.save(netD.state_dict(), '%s/netD_%04d.pth' % (opt.outf, epoch))
+            optimizerD.zero_grad()
+
+            noise = torch.randn(opt.batchSize, opt.nz)
+
+            noise = noise.to(device)
+
+            fake = netG(noise)  # 生成假图
+            outputfake = netD(fake.detach())  # 避免梯度传到G，因为G不用更新
+            lossD = criterion(outputreal, outputfake)
+            lossD.backward()
+            optimizerD.step()
+
+            # 固定鉴别器D，训练生成器G
+            optimizerG.zero_grad()
+
+            output = netD(fake)
+            lossG = criterion(output)
+            lossG.backward()
+            optimizerG.step()
+
+            pbar.set_postfix(**{'lossD': lossD.item(),
+                                'lrd': get_lr(optimizerD), 'lossG': lossG.item(),
+                                'lrg': get_lr(optimizerG)})
+            pbar.update(1)
+
+        #一个epoch
+        vutils.save_image(fake.data,
+                          '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
+                          normalize=True)
+        lrd_scheduler.step()
+        lrg_scheduler.step()
+
+        if epoch % 5 == 0:#每5个epoch，保存一次模型参数.
+
+            checkpointD = {
+                "net": netD.state_dict(),
+                'optimizer': optimizerD.state_dict(),
+                "epoch": epoch
+            }
+            checkpointG = {
+                "net": netG.state_dict(),
+                'optimizer': optimizerG.state_dict(),
+                "epoch": epoch
+            }
+            if not os.path.isdir("./checkpoint"):
+                os.mkdir("./checkpoint")
+            torch.save(checkpointG, './checkpoint/ckpt_latestG_%s.pth' % (str(epoch)))
+            torch.save(checkpointD, './checkpoint/ckpt_latestD_%s.pth' % (str(epoch)))
+
+
